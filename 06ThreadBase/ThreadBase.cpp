2fe78586cd6992dab6026b723d1614f49b27e040
kris hao <76926060+kris-zsh@users.noreply.github.com>
2023-01-05 22:16:02 +0800

    Suspend and Resume Thread

--- 06ThreadBase/ThreadBase.cpp
+++ 06ThreadBase/ThreadBase.cpp
@@ -1,6 +1,7 @@
 #include <windows.h>
 #include <tchar.h>
 #include <iostream>
+#include <TlHelp32.h>
 
 using namespace std;
 
@@ -14,14 +15,36 @@ DWORD WINAPI func(PVOID szParam)
 
 	return 0;
 }
+void MySuspendThread(DWORD dwProcessId, BOOL isSuspend)
+{
+	HANDLE hSnapShot = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
+
+	THREADENTRY32 te = { sizeof(te) };
+	BOOL re = Thread32First(hSnapShot, &te);
+
+	for (; re; re = Thread32Next(hSnapShot, &te))
+	{
+		if (te.th32OwnerProcessID == dwProcessId)
+		{
+			HANDLE hThread = OpenThread(THREAD_ALL_ACCESS, FALSE, te.th32ThreadID);
+			if (!hThread)
+			{
+				wcout << "OpenThreadId: " << te.th32ThreadID << " fail GEL: " << GetLastError() << endl;
+				continue;
+			}
+			if (isSuspend)
+				SuspendThread(hThread);
+			else
+				ResumeThread(hThread);
+			wcout << "ThreadID: " << te.th32ThreadID << endl;
+			CloseHandle(hThread);
+		}
+	}
+	CloseHandle(hSnapShot);
+}
 int main(int argc, char* argv[])
 {
 	std::wcout.imbue(std::locale("chs"));
 
-	int i = 10;
-	HANDLE hProcess = GetCurrentProcess();
-	DuplicateHandle(GetCurrentProcess(), GetCurrentProcess(), GetCurrentProcess(), &hProcess, 0, FALSE, DUPLICATE_SAME_ACCESS);
-
-	HANDLE hTread = CreateThread(nullptr, 0, func, hProcess, 0, nullptr);
-	WaitForSingleObject(hTread, INFINITE);
+	MySuspendThread(42260, FALSE);
 }
