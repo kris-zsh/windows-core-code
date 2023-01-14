969383f8d921f05f896128d8cb315314b621f8da
kris hao <76926060+kris-zsh@users.noreply.github.com>
2023-01-14 23:34:20 +0800

    IOCompletePort

--- 10-CopyFIle/FileCopy.cpp
+++ 10-CopyFIle/FileCopy.cpp
@@ -202,21 +202,20 @@ MyGetOverlappedResult(
 	_In_ BOOL bWait
 )
 {
-	
 	DWORD dwRet = WAIT_TIMEOUT;
-	if(lpOverlapped->Internal == STATUS_PENDING)
+	if (lpOverlapped->Internal == STATUS_PENDING)
 	{
-		if(bWait)
+		if (bWait)
 		{
 			HANDLE hHandle = lpOverlapped->hEvent == nullptr ? hFile : lpOverlapped->hEvent;
 			dwRet = WaitForSingleObject(hHandle, INFINITE);
 		}
-		if(dwRet == WAIT_TIMEOUT)
+		if (dwRet == WAIT_TIMEOUT)
 		{
 			SetLastError(ERROR_IO_INCOMPLETE);
 			return (FALSE);
 		}
-		if(dwRet != WAIT_OBJECT_0)
+		if (dwRet != WAIT_OBJECT_0)
 		{
 			return (FALSE);
 		}
@@ -230,9 +229,258 @@ MyGetOverlappedResult(
 	}
 }
 
+#define BUFFSIZE (64 * 1024)
+#define MAX_COUNT 4
+
+enum CompeletKey
+{
+	CW_READ = 0,
+	CW_WRITE
+};
+
+using PENSURECLEANUP = void (WINAPI *)(UINT_PTR);
+
+template <class TYPE, PENSURECLEANUP pfn, UINT_PTR tInvaild = 0>
+class EnsureCleanUp
+{
+	UINT_PTR _mt;
+
+public:
+	EnsureCleanUp() { _mt = tInvaild; }
+
+	EnsureCleanUp(TYPE t) : _mt(UINT_PTR(t))
+	{
+	}
+
+	~EnsureCleanUp() { CleanUp(); }
+
+	BOOL isValid() { return _mt != tInvaild; }
+	BOOL isInvalid() { return !isValid(); }
+
+	void CleanUp()
+	{
+		if (isValid())
+		{
+			pfn(_mt);
+			_mt = tInvaild;
+		}
+	}
+
+	TYPE operator =(TYPE t)
+	{
+		CleanUp();
+		_mt = static_cast<UINT_PTR>(t);
+		return (*this);
+	}
+
+	operator TYPE()
+	{
+		return TYPE(_mt);
+	}
+};
+
+#define MakeCleanupClassX(className, pfnCleanUp, dataType, tInvaild)\
+	using className = EnsureCleanUp<dataType, (PENSURECLEANUP)pfnCleanUp, (UINT_PTR)tInvaild>
+
+
+MakeCleanupClassX(FileCleanUp, CloseHandle, HANDLE, INVALID_HANDLE_VALUE);
+
+
+class CIOReq : public OVERLAPPED
+{
+	size_t m_BuffSize;
+	PVOID p_data;
+
+public:
+	CIOReq()
+	{
+		Offset = 0;
+		OffsetHigh = 0;
+		hEvent = nullptr;
+		Internal = 0;
+		InternalHigh = 0;
+		m_BuffSize = 0;
+		p_data = nullptr;
+	}
+
+	~CIOReq()
+	{
+		VirtualFree(p_data, 0, MEM_RELEASE);
+	}
+
+	BOOL AllocSpace(size_t Buffsize)
+	{
+		m_BuffSize = Buffsize;
+		p_data = VirtualAlloc(nullptr, m_BuffSize, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+		return p_data != nullptr;
+	}
+
+	BOOL Read(HANDLE hDevice, PLARGE_INTEGER li = nullptr)
+	{
+		if (li)
+		{
+			Offset = li->LowPart;
+			OffsetHigh = li->HighPart;
+		}
+
+		return (ReadFile(hDevice, p_data, m_BuffSize, nullptr, this));
+	}
+
+	BOOL Write(HANDLE hDevice, PLARGE_INTEGER li = nullptr)
+	{
+		if (li)
+		{
+			Offset = li->LowPart;
+			OffsetHigh = li->HighPart;
+		}
+
+		return WriteFile(hDevice, p_data, m_BuffSize, nullptr, this);
+	}
+};
+
+class CIOCP
+{
+	HANDLE hIOCP;
+public:
+
+	CIOCP(int maxConcurrentCnt = -1)
+	{
+		hIOCP = nullptr;
+		if (maxConcurrentCnt != -1)
+			Create(maxConcurrentCnt);
+	}
+	~CIOCP()
+	{
+		if (hIOCP)
+			CloseHandle(hIOCP);
+	}
+
+	void Create(int max_concurrent_cnt)
+	{
+		hIOCP = CreateIoCompletionPort(INVALID_HANDLE_VALUE, nullptr, 0, max_concurrent_cnt);
+	}
+
+	void AssociateDevice(HANDLE hDevice, ULONG_PTR uKey)
+	{
+		CreateIoCompletionPort(hDevice, hIOCP, uKey, 0);
+	}
+	
+	BOOL GetStatus(LPDWORD lpNumberOfBytesTransferred, PULONG_PTR lpCompletionKey, 
+		LPOVERLAPPED* lpOverlapped, DWORD dwMilliseconds = INFINITE)
+	{
+		return GetQueuedCompletionStatus(hIOCP, lpNumberOfBytesTransferred, lpCompletionKey,
+			lpOverlapped, dwMilliseconds);
+	}
+
+	BOOL PostStatus(DWORD dwNumberOfBytesTransferred, 
+		ULONG_PTR dwCompletionKey = 0, LPOVERLAPPED lpOverlapped = nullptr)
+	{
+		return PostQueuedCompletionStatus(hIOCP, dwNumberOfBytesTransferred, dwCompletionKey, lpOverlapped);
+	}
+};
+
+template <class MT, class MV>
+MT RoundDown(MT szval, MV multiply)
+{
+	MT temp = (szval / multiply) * multiply;
+	temp += (szval % multiply) ? multiply : 0;
+	return temp;
+}
+
+BOOL MyFileCopy(PCTSTR pszSrc, PCTSTR pszDest)
+{
+	LARGE_INTEGER liSrc = {}, liDest = {};
+	BOOL Ok = FALSE;
+	DWORD loopCnt = 0;
+
+	try
+	{
+		{
+			FileCleanUp hSrc = CreateFile(pszSrc, GENERIC_READ, FILE_SHARE_READ, nullptr,
+				OPEN_EXISTING, FILE_FLAG_NO_BUFFERING | FILE_FLAG_OVERLAPPED, nullptr);
+			if (hSrc.isInvalid()) goto leave;
+
+			GetFileSizeEx(hSrc, &liSrc);
+			liDest.QuadPart = RoundDown(liSrc.QuadPart, BUFFSIZE);
+
+			FileCleanUp hDest = CreateFile(pszDest, GENERIC_WRITE, 0, nullptr,
+				CREATE_ALWAYS, FILE_FLAG_NO_BUFFERING | FILE_FLAG_OVERLAPPED,hSrc);
+			if (hDest.isInvalid()) goto leave;
+
+			SetFilePointerEx(hDest, liDest, nullptr, FILE_BEGIN);
+			SetEndOfFile(hDest);
+
+			CIOCP iocp(0);
+			iocp.AssociateDevice(hDest, CompeletKey::CW_WRITE);
+			iocp.AssociateDevice(hSrc, CompeletKey::CW_READ);
+
+			DWORD dwReadInProcess = {}, dwWriteInProcess = {};
+			LARGE_INTEGER liCur = {};
+
+			CIOReq IOR[MAX_COUNT] = {};for(int i = 0; i < _countof(IOR); i++)
+			{
+				IOR[i].AllocSpace(BUFFSIZE);
+				dwWriteInProcess++;
+				iocp.PostStatus(0, CompeletKey::CW_WRITE, &IOR[i]);
+			}
+			while( dwReadInProcess > 0 | dwWriteInProcess > 0)
+			{
+				BOOL bResult = FALSE;
+				CIOReq* IOR = {};
+				DWORD dwByte = {};
+				ULONG_PTR CompleteKey = {};
+
+
+				bResult = iocp.GetStatus(&dwByte, &CompleteKey, (LPOVERLAPPED*)&IOR);
+				switch (CompleteKey)
+				{
+				case CompeletKey::CW_WRITE:
+					dwWriteInProcess--;
+					if(liCur.QuadPart < liDest.QuadPart)
+					{
+						dwReadInProcess++;
+						bResult = IOR->Read(hSrc, &liCur);
+						liCur.QuadPart += BUFFSIZE;
+					}
+					break;
+				case CompeletKey::CW_READ:
+					dwWriteInProcess++;
+					dwReadInProcess--;
+					bResult = IOR->Write(hDest);
+					break;
+				}
+				loopCnt++;
+			}
+			Ok = TRUE;
+		}
+	leave:;
+	}catch (...)
+	{
+		
+	}
+	FileCleanUp hFile = CreateFile(pszDest, GENERIC_WRITE, 0, nullptr, OPEN_EXISTING,
+		0, nullptr);
+	if(hFile.isValid())
+	{
+		SetFilePointerEx(hFile, liSrc, nullptr, FILE_BEGIN);
+		SetEndOfFile(hFile);
+	}
+	cout << "while loopcnt: " << loopCnt << endl;
+	return TRUE;
+}
 int main()
 {
 	std::wcout.imbue(std::locale("chs"));
 
+	// FileCleanUp hFile = CreateFileA("test.zsh", GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ, nullptr, OPEN_ALWAYS,
+	// 	FILE_FLAG_OVERLAPPED, nullptr);
+	// if (hFile == INVALID_HANDLE_VALUE)
+	// {
+	// 	cout << "GLE: " << GetLastError() << endl;
+	// 	return 0;
+	//
+	// }
+	MyFileCopy(LR"(D:\Ñ¸À×ÏÂÔØ\[www.domp4.cc]µØxÒýl.2013.BD1080p.¹úÓ¢Ë«Óï.ÖÐÓ¢Ë«×Ö (1).mp4)",
+		LR"(D:\Ñ¸À×ÏÂÔØ\[www.domp4.cc]µØxÒýl.2013.BD1080p.¹úÓ¢Ë«Óï.ÖÐÓ¢Ë«×Ö (1).mp4_1)");
 	return 0;
 }
